4ee8ff3a637fe3c8282bddcd9f134d7eeaa59212
Haile-08 <haile4cmd@gmail.com>
2022-11-13 04:51:56 -0800

    Create the string length, shell execute and string concat function

--- _strlen.c
+++ _strlen.c
@@ -1,2 +1,17 @@
 #include "main.c"
 
+/**
+ * _strlen - string length
+ * Description: return the length of a string
+ * @s: string pointer value
+ *
+ * Return: string_length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
--- main.h
+++ main.h
@@ -9,6 +9,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+char *shell_read_line(char *av);
+char **shell_parser(char *line, char *av);
+int shell_execute(char *const args, char *av);
+
+/* custome function */
+
 int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
+int _strlen(char *s);
 
 #endif
