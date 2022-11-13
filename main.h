87e6f28595ae61a66bb9047e14451b4cd2d3afc9
Haile-08 <haile4cmd@gmail.com>
2022-11-13 04:05:02 -0800

    Creat the header file main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_HEADER_
+#define _MAIN_HEADER_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int _putchar(char c);
+
+#endif
